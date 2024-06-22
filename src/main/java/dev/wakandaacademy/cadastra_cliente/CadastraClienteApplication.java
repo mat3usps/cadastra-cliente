b71c69258b5822801afa7244e7755204e5e7526e
Mateus Pereira <mateusp.s@outlook.com>
2024-06-22 17:13:39 -0300

    init commit

---
+++ src/main/java/dev/wakandaacademy/cadastra_cliente/CadastraClienteApplication.java
@@ -0,0 +1,15 @@
+package dev.wakandaacademy.cadastra_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastraClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastraClienteApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/dev/wakandaacademy/cadastra_cliente/CadastraClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.cadastra_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastraClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
