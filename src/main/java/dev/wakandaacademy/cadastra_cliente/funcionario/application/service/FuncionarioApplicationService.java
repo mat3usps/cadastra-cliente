e7ab72f67f2ae1195badf2104da0badbf1042ec1
Mateus Pereira <mateusp.s@outlook.com>
2024-06-23 19:22:04 -0300

    refactor: change of portuguese code

--- src/main/java/dev/wakandaacademy/cadastra_cliente/funcionario/application/service/FuncionarioApplicationService.java
+++ src/main/java/dev/wakandaacademy/cadastra_cliente/funcionario/application/service/FuncionarioApplicationService.java
@@ -2,6 +2,7 @@ package dev.wakandaacademy.cadastra_cliente.funcionario.application.service;
 
 import dev.wakandaacademy.cadastra_cliente.funcionario.application.api.FuncionarioIdResponse;
 import dev.wakandaacademy.cadastra_cliente.funcionario.application.api.FuncionarioRequest;
+import dev.wakandaacademy.cadastra_cliente.funcionario.application.repository.FuncionarioRepository;
 import dev.wakandaacademy.cadastra_cliente.funcionario.domain.Funcionario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -15,7 +16,7 @@ public class FuncionarioApplicationService implements FuncionarioService {
     @Override
     public FuncionarioIdResponse createNewFuncionario(FuncionarioRequest funcionarioRequest) {
         log.info("[start] - FuncionarioService - createNewFuncionario");
-        Funcionario newFuncionario = funcionarioRepository.salva(new Funcionario(funcionarioRequest));
+        Funcionario newFuncionario = funcionarioRepository.save(new Funcionario(funcionarioRequest));
         log.info("[finish] - FuncionarioService - createNewFuncionario");
         return FuncionarioIdResponse.builder().idFuncionario(newFuncionario.getIdFuncionario()).build();
     }
